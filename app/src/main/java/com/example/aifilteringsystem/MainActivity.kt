1102c0030f1eb04fe536f4c25b9b548532b2d69c
Code <taste397@naver.com>
2022-09-07 19:11:09 +0900

    commit

---
+++ app/src/main/java/com/example/aifilteringsystem/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.aifilteringsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    var urlText: ArrayList<EditText>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.main_recyclerView)
+        recyclerView.adapter = RecyclerViewAdapter(layoutInflater)
+
+        findViewById<AppCompatButton>(R.id.plus_button).setOnClickListener {
+
+        }
+
+        findViewById<AppCompatButton>(R.id.minus_button).setOnClickListener {
+
+        }
+    }
+
+    class RecyclerViewAdapter(
+        val inflater: LayoutInflater
+    ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val urlEditText = itemView.findViewById<EditText>(R.id.url_edit)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(inflater.inflate(R.layout.url_item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return 0
+        }
+    }
+}
