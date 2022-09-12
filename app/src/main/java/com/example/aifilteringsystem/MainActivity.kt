55bfc560b17a23e3d672faec2bcddf4d22e60d07
cotton <taste397@naver.com>
2022-09-12 14:47:05 +0900

    commit

--- app/src/main/java/com/example/aifilteringsystem/MainActivity.kt
+++ app/src/main/java/com/example/aifilteringsystem/MainActivity.kt
@@ -2,50 +2,37 @@ package com.example.aifilteringsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import androidx.appcompat.widget.AppCompatButton
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.main_recyclerView)
-        recyclerView.adapter = RecyclerViewAdapter(layoutInflater)
+        val pager = findViewById<ViewPager2>(R.id.main_pager)
+        pager.adapter = MainPagerAdapter(this, 1)
+    }
+}
 
-//        recyclerView.adapter.notifyDataSetChanged()
 
-        findViewById<AppCompatButton>(R.id.plus_button).setOnClickListener {
-
-        }
-
-        findViewById<AppCompatButton>(R.id.minus_button).setOnClickListener {
-
-        }
+class MainPagerAdapter(
+    fragmentActivity: FragmentActivity,
+    val tabCount: Int,
+) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        return tabCount
     }
 
-    class RecyclerViewAdapter(
-        val inflater: LayoutInflater
-    ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val urlEditText = itemView.findViewById<EditText>(R.id.url_edit)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(inflater.inflate(R.layout.url_item, parent, false))
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        }
-
-        override fun getItemCount(): Int {
-            return 0
-        }
+    override fun createFragment(position: Int): Fragment {
+        return MainFragment()
+        /*
+        when (position) {
+            0 -> return InstaFeedFragment()
+            1 -> return instaPostFragment
+            else -> return InstaProfileFragment()
+        }*/
     }
 }
